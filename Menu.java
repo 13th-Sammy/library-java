1d60bed3d735b2e97243cbb95ceb5e21b7b2ac99
13th-Sammy <213174535+13th-Sammy@users.noreply.github.com>
2025-06-27 22:19:51 +0530

    Update

--- Menu.java
+++ Menu.java
@@ -14,63 +14,54 @@ class Menu {
         JButton viewBooksBtn=new JButton("View Books");
         viewBooksBtn.addActionListener(e -> {
             lib.viewBooks();
-            JOptionPane.showMessageDialog(frame, "View Books clicked");
         });
         frame.add(viewBooksBtn);
 
         JButton addBookBtn=new JButton("Add Book");
         addBookBtn.addActionListener(e -> {
             lib.addBook();
-            JOptionPane.showMessageDialog(frame, "Add Book clicked");
         });
         frame.add(addBookBtn);
 
         JButton remBookBtn=new JButton("Remove Book");
         remBookBtn.addActionListener(e -> {
             lib.removeBook();
-            JOptionPane.showMessageDialog(frame, "Remove Book clicked");
         });
         frame.add(remBookBtn);
 
         JButton searchBookBtn=new JButton("Search Book");
         searchBookBtn.addActionListener(e-> {
             lib.searchBook();
-            JOptionPane.showMessageDialog(frame, "Search Book clicked");
         });
         frame.add(searchBookBtn);
 
         JButton regUserBtn=new JButton("Register User");
         regUserBtn.addActionListener(e -> {
             lib.registerUser();
-            JOptionPane.showMessageDialog(frame, "Register User clicked");
         });
         frame.add(regUserBtn);
 
         JButton viewUserBtn=new JButton("View User");
         viewUserBtn.addActionListener(e-> {
             lib.viewUser();
-            JOptionPane.showMessageDialog(frame, "View User clicked");
         });
         frame.add(viewUserBtn);
 
         JButton viewAllUsersBtn=new JButton("View All Users");
         viewAllUsersBtn.addActionListener(e -> {
             lib.viewAllUsers();
-            JOptionPane.showMessageDialog(frame, "View All Users clicked");
         });
         frame.add(viewAllUsersBtn);
 
         JButton issueBookBtn=new JButton("Issue Book");
         issueBookBtn.addActionListener(e-> {
             lib.issueBook();
-            JOptionPane.showMessageDialog(frame, "Issue Book clicked");
         });
         frame.add(issueBookBtn);
 
         JButton returnBookBtn=new JButton("Return Book");
         returnBookBtn.addActionListener(e -> {
             lib.returnBookL();
-            JOptionPane.showMessageDialog(frame, "Return Book clicked");
         });
         frame.add(returnBookBtn);
     }
