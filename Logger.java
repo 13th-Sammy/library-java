57c6030ea13f9d968b1e155c1629cd3eaec6e3b5
13th-Sammy <213174535+13th-Sammy@users.noreply.github.com>
2025-06-28 21:36:49 +0530

    Update

--- Logger.java
+++ Logger.java
@@ -7,10 +7,4 @@ class Logger {
         String timestamp=java.time.LocalDateTime.now().toString().trim();
         logs.add("[" + timestamp + "] " + s);
     }
-
-    public void printLogs() {
-        for(String l:logs) {
-            System.out.println(l);
-        }
-    }
 }
